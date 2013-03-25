a48a0dbd65392be620e14a4080a9f7e40e09de0d
Arnas L <arnaslu@gmail.com>
2013-03-25 16:42:39 +0200

    Updating TicketManager to implement Zendeskmanager

--- lib/ZendeskManager/TicketManager.php
+++ lib/ZendeskManager/TicketManager.php
@@ -2,40 +2,11 @@
 
 namespace ZendeskManager;
 
-use Buzz\Browser;
-use ZendeskManager\Exception;
-
-class TicketManager implements TicketManagerInterface
+class TicketManager extends ZendeskManager implements TicketManagerInterface
 {
-
     const TICKET_LIST = '/api/v2/tickets.json';
     const TICKET_CREATE = '/api/v2/tickets.json';
     const TICKET_WEB_URL = '/agent/#/tickets/%id%';
-
-    /**
-     * @var Browser
-     */
-    protected $browser;
-    protected $url;
-
-    public function __construct($browser)
-    {
-        if (!$this->_isCurlInstalled()) {
-            throw new Exception\MissingCurlException();
-        }
-        
-        $this->browser = $browser;
-    }
-    
-    public function setUrl($url)
-    {
-        $this->url = $url;
-    }
-    
-    public function setUsernamePassword($userPass)
-    {
-        $this->browser->getClient()->setOption(CURLOPT_USERPWD, $userPass);
-    }
 
     public function generateWebUrlForTicket(TicketInterface $ticket)
     {
@@ -61,43 +32,6 @@ class TicketManager implements TicketManagerInterface
         );
 
         return new Ticket($this->getResponse()->ticket);
-    }
-
-    protected function getResponse()
-    {
-        $content = $this->browser->getLastResponse()->getContent();
-        $response = json_decode($this->browser->getLastResponse()->getContent());
-
-        if (isset($response->error)) {
-            if ($response->error == 'RecordInvalid') {
-                throw new Exception\RecordInvalidException($content);
-            } else {
-                throw new Exception\UnexpectedResponseException($content);
-            }
-        } elseif ($this->getResponseStatus() >= 300) {
-            throw new Exception\UnexpectedResponseException($content);
-        }
-
-        return $response;
-    }
-
-    protected function getResponseStatus()
-    {
-        $headers = $this->browser->getLastResponse()->getHeaders();
-        foreach ($headers as $header) {
-            if (substr($header, 0, 6) == 'Status') {
-                return (int) substr($header, 8, 3);
-            }
-        }
-    }
-
-    protected function _isCurlInstalled()
-    {
-        if (in_array('curl', get_loaded_extensions())) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
 }
